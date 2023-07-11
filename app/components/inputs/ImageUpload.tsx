59ef2c6f2181edd22de90864a1be3abd60ee2d80
acascioli <alexcascioli@hotmail.it>
2023-07-11 19:01:07 +0200

    implemented add your home

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -9,7 +9,7 @@ declare global {
   var cloudinary: any;
 }
 
-const uploadPreset = "pgc9ehd5";
+const uploadPreset = "v1kavynl";
 
 interface ImageUploadProps {
   onChange: (value: string) => void;
